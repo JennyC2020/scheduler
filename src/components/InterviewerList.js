2e44ff6ee7d176037c23a1b5234c7778fc7cbd7d
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-01 13:33:12 -0700

    Add Interview List and refract to perform props

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem'
+
+export default function InterviewerList({ interviewers, interviewer, setInterviewer }) {
+
+  const interviewerData = interviewers.map(({ id, name, avatar }) => {
+    return <InterviewerListItem
+      key={id}
+      name={name}
+      avatar={avatar}
+      selected={id === interviewer}
+      setInterviewer={() => setInterviewer(id)}
+    />
+  })
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewerData}
+      </ul>
+    </section>
+  );
+
+}

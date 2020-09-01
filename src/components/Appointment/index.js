2bc6c95e82074f7ec7661f56bfe3c3b023b72244
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-01 15:48:55 -0700

    Fix appointment booked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import './styles.scss';
-import Header from './Header'
-import Show from './Show'
-import Empty from './Empty'
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+import './styles.scss'
 
 export default function Appointment(props) {
-
   return (
-    <article className='appointment'>
+    <article className="appointment">
       <Header
         time={props.time}
       />
       {props.interview ?
         <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer.name}
+          interviewer={props.interview.interviewer}
         /> : <Empty />}
     </article>
   );
-
-
-}
+};

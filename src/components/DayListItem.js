54896eef0b041f03038a343ab69e0c46e730e5bb
Jenny Citherlet <jcitherlet@cendyn.com>
2020-08-31 17:18:01 -0700

    Add new formatSpots function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,24 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full ": props.spots === 0
   });
+
+  const formatSpots = (spots) => {
+    if (!spots) {
+      return 'no spots remaining';
+    } else if (spots === 1) {
+      return '1 spot remaining';
+    }
+    return `${spots} spots remaining`;
+  }
+
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+    <li
+      onClick={() => props.setDay(props.name)}
+      className={dayClass}
+    >
+      <h2 className='text--regular'>{props.name}</h2>
+      <h3 className='text--light'>{formatSpots(props.spots)}</h3>
     </li>
   );
+
 }

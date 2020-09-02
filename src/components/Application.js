92484865a9701d21752ba22675a071c628c3005b
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-02 16:46:26 -0700

    Remove unused code

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,23 +5,6 @@ import Appointment from 'components/Appointment';
 
 const axios = require('axios');
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
 
 const appointments = [
   {

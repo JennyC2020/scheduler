919acf8d37bc6e2fe9a66b5a7c6c00abec89f1ce
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-02 14:53:30 -0700

    Add Axios get req to API

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import DayList from 'components/DayList';
 import "components/Application.scss";
 import Appointment from 'components/Appointment';
+
+const axios = require('axios');
 
 const days = [
   {

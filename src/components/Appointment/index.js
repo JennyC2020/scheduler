384168e92dcc08fc86f1c69f270682bf6d41273b
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-03 20:05:15 -0700

    Add new props to Appointement

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import './styles.scss'
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
-import './styles.scss'
+import Form from './form';
+import { useVisualMode } from '../../hooks/useVisualMode';
 
 export default function Appointment(props) {
+
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header
         time={props.time}
       />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-        /> : <Empty />}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 };

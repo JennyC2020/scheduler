e7544efd469bf7e0db8ce197b875fa9093f6a61f
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-07 15:14:37 -0700

    Add test for Form component

--- src/components/Appointment/form.js
+++ src/components/Appointment/form.js
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
-import './styles.scss';
+import React, {useState} from "react";
+import "./styles.scss";
 import Button from "components/Button.js";
 import InterviewerList from "components/InterviewerList.js";
 
 export default function Form(props) {
-
-  const [name, setName] = useState(props.name || '');
+  const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   const reset = () => {
-    setName('');
+    setName("");
     setInterviewer(null);
-  }
+  };
 
   const cancel = () => {
     reset();
     props.onCancel();
-  }
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             value={name}
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
         <InterviewerList
@@ -38,11 +38,14 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={cancel} danger>
+            Cancel
+          </Button>
+          <Button onClick={() => props.onSave(name, interviewer)} confirm>
+            Save
+          </Button>
         </section>
       </section>
     </main>
   );
-
 }

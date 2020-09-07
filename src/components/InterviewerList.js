ab294cd5e81b9842baf8535975912700193faaad
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-07 13:57:36 -0700

    Add prop types for testing

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React from "react";
 import "components/InterviewerList.scss";
-import InterviewerListItem from './InterviewerListItem'
+import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from "prop-types";
 
-export default function InterviewerList({ interviewers, interviewer, setInterviewer }) {
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
 
-  const interviewerData = interviewers.map(({ id, name, avatar }) => {
-    return <InterviewerListItem
-      key={id}
-      name={name}
-      avatar={avatar}
-      selected={id === interviewer}
-      setInterviewer={() => setInterviewer(id)}
-    />
-  })
+export default function InterviewerList({
+  interviewers,
+  interviewer,
+  setInterviewer,
+}) {
+  const interviewerData = interviewers.map(({id, name, avatar}) => {
+    return (
+      <InterviewerListItem
+        key={id}
+        name={name}
+        avatar={avatar}
+        selected={id === interviewer}
+        setInterviewer={() => setInterviewer(id)}
+      />
+    );
+  });
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">
-        {interviewerData}
-      </ul>
+      <ul className="interviewers__list">{interviewerData}</ul>
     </section>
   );
-
 }

c1baafbd139b95fdbb8ead6d18f86590bd72b562
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-08 12:07:14 -0700

    Fix form test bug

--- src/components/Appointment/form.js
+++ src/components/Appointment/form.js
@@ -8,55 +8,56 @@ export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [error, setError] = useState("");
 
-  // Clear form when user presses cancel
-  function reset() {
+  const reset = () => {
     setName("");
     setInterviewer(null);
-    props.onCancel();
-  }
+  };
 
-  // Validate that student name is present and interviewer has been selected before saving
-  // Else, display descriptive error message
-  function validate() {
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  function validate(name, interviewer) {
     if (name === "") {
       setError("Student name cannot be blank");
       return;
     }
+
     setError("");
-    if (!interviewer) {
-      setError("Please select interviewer");
-      return;
-    }
     props.onSave(name, interviewer);
   }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
-            value={name}
             type="text"
             placeholder="Enter Student Name"
-            onChange={(e) => setName(e.target.value)}
+            value={name}
+            onChange={(event) => {
+              event.persist();
+              setName(event.target.value);
+            }}
             data-testid="student-name-input"
           />
           <section className="appointment__validation">{error}</section>
-          <InterviewerList
-            interviewers={props.interviewers}
-            value={interviewer}
-            onChange={setInterviewer}
-          />
         </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={reset}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
-          <Button confirm onClick={validate}>
+          <Button confirm onClick={() => validate(name, interviewer)}>
             Save
           </Button>
         </section>

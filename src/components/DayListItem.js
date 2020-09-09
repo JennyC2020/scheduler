9a01edcdeced804eb47e244ecc09a9c14c2873cb
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-09 13:27:34 -0700

    Fix application test debug typos

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,30 +1,30 @@
 import React from "react";
 import "components/DayListItem.scss";
-import classNames from 'classnames/bind';
+import classNames from "classnames/bind";
 
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full ": props.spots === 0
+    "day-list__item--full ": props.spots === 0,
   });
 
   const formatSpots = (spots) => {
     if (!spots) {
-      return 'no spots remaining';
+      return "no spots remaining";
     } else if (spots === 1) {
-      return '1 spot remaining';
+      return "1 spot remaining";
     }
     return `${spots} spots remaining`;
-  }
+  };
 
   return (
     <li
       onClick={() => props.setDay(props.name)}
       className={dayClass}
+      data-testid="day"
     >
-      <h2 className='text--regular'>{props.name}</h2>
-      <h3 className='text--light'>{formatSpots(props.spots)}</h3>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
-
 }
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -29,7 +29,7 @@ describe("Application", () => {
   });
 
   it("Loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const {container, debug} = render(<Application />);
+    const {container} = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -45,7 +45,7 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
 
-    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    expect(getByText(appointment, "Saving...")).toBeInTheDocument();
 
     await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
 
@@ -53,36 +53,36 @@ describe("Application", () => {
       queryByText(day, "Monday")
     );
 
-    console.log("day", prettyDOM(day));
+    // console.log("day", prettyDOM(day));
 
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
-    const {container, debug} = render(<Application />);
+    const {container} = render(<Application />);
 
     // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
     // 3. Click the 'Delete' button on the booked appt.
-    const appointment = getAllByTestId(
-      container,
-      "appointment"
-    ).find((appointment) => queryByText(appointment, "Archie Cohen"));
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments.find((appointment) =>
+      queryByText(appointment, "Archie Cohen")
+    );
 
-    fireEvent.click(queryByAltText(appointment, "Delete"));
+    fireEvent.click(getByAltText(appointment, "Delete"));
 
     // 4. Check that the confirmation message is shown
     expect(
-      getByText(appointment, "Are you sure you would like to delete?")
+      getByText(appointment, "Are you sure you would like to delete this?")
     ).toBeInTheDocument();
 
     // 5. Confirm delete by clicking 'Confirm'
     fireEvent.click(queryByText(appointment, "Confirm"));
 
-    // 6. Check that the element with the text 'Deleting' is displayed
-    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    // 6. Check that the element with the text 'Deleting...' is displayed
+    expect(getByText(appointment, "Deleting...")).toBeInTheDocument();
 
     // 7. Wait until the element with the "Add" button is displayed.
     await waitForElement(() => getByAltText(appointment, "Add"));
@@ -96,7 +96,7 @@ describe("Application", () => {
   });
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
-    const {container, debug} = render(<Application />);
+    const {container} = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -114,15 +114,13 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
 
-    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    expect(getByText(appointment, "Saving...")).toBeInTheDocument();
 
     await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
 
     const day = getAllByTestId(container, "day").find((day) =>
       queryByText(day, "Monday")
     );
-
-    console.log("day", prettyDOM(day));
 
     expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
   });
